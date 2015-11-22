fd4396d40a5cd86a346b044e23036b2235f5cdf5
Fernando Gabrieli <fgabrieli@gmail.com>
2015-11-22 18:47:38 -0300

    Removing old files

--- base/Builder.js
+++
@@ -1,6 +0,0 @@
-// Builder abstract
-var Builder = new Function();
-
-Builder.prototype.build = function() {
-  // return new instance of part
-}
--- base/Factory.js
+++
@@ -1,6 +0,0 @@
-// Factory abstract
-var Factory = new Function();
-
-Factory.prototype.make = function() {
-  // return new instance of proper product
-}
